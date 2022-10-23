aab955e2123b1259da88745e5c35666c0427db91
David Rollinson <g7gpr@outlook.com>
2022-10-23 09:46:43 +0800

    Added gmnmultitrack

---
+++ gmnmultitrack.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+#This script will run a track stack for all the cameras on this system
+
+thiscamera=au000u
+for thiscamerapath in /home/gmn/cameras/*
+do
+thiscamera=`echo $thiscamerapath | cut -d'/' -f5-`
+captured=/home/$thiscamera/RMS_data/CapturedFiles/`ls /home/$thiscamera/RMS_data/CapturedFiles/ | tail -n1 | head -n1`
+echo $captured
+commandstring=$commandstring'   '$captured
+captured=/home/$thiscamera/RMS_data/CapturedFiles/`ls /home/$thiscamera/RMS_data/CapturedFiles/ | tail -n2 | head -n1`
+echo $captured
+commandstring=$commandstring'   '$captured
+captured=/home/$thiscamera/RMS_data/CapturedFiles/`ls /home/$thiscamera/RMS_data/CapturedFiles/ | tail -n3 | head -n1`
+echo $captured
+commandstring=$commandstring'   '$captured
+captured=/home/$thiscamera/RMS_data/CapturedFiles/`ls /home/$thiscamera/RMS_data/CapturedFiles/ | tail -n4 | head -n1`
+echo $captured
+commandstring=$commandstring'   '$captured
+captured=/home/$thiscamera/RMS_data/CapturedFiles/`ls /home/$thiscamera/RMS_data/CapturedFiles/ | tail -n5 | head -n1`
+echo $captured
+commandstring=$commandstring'   '$captured
+captured=/home/$thiscamera/RMS_data/CapturedFiles/`ls /home/$thiscamera/RMS_data/CapturedFiles/ | tail -n6 | head -n1`
+echo $captured
+commandstring=$commandstring'   '$captured
+done
+commandstring='python -m Utils.TrackStack '$commandstring'  -c . -o /home/gmn/outbox -s'$1
+echo $commandstring
