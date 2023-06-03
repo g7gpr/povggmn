70c74af8df2a0f8e26b4d159e2068efa8cab6043
David Rollinson <g7gpr@outlook.com>
2023-06-03 17:21:53 +0000

    Different repo

--- gmndifferentrepo.sh
+++ gmndifferentrepo.sh
@@ -9,11 +9,14 @@ cd ~/source
 mv ~/source/RMS ~/source/RMS_backup
 git clone https://github.com/g7gpr/RMS
 cd ~/source/RMS
-cp ~/gmnbackup/$(whoami)/platepar_cmn2010.cal .
-cp ~/gmnbackup/$(whoami)/mask.bmp .
 Scripts/RMS_Update.sh
 git remote -v
 rm ~/source/RMS/.config
+rm ~/source/RMS/mask.bmp
+rm ~/source/RMS/platepar_cmn2010.cal
 git checkout eventmonitor
 cp ~/gmnbackup/$(whoami)/.config .
+cp ~/gmnbackup/$(whoami)/mask.cmp . 
+cp ~/gmnbackup/$(whoami)/platepar_cmn2010.cal . 
+
 git branch
