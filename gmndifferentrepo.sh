41ea6a7e25b4361bc7bbae3ae1daa5e4b6422ebd
David Rollinson <g7gpr@outlook.com>
2023-06-03 16:32:54 +0000

    improved gmndifferentrepo

--- gmndifferentrepo.sh
+++ gmndifferentrepo.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 echo Moving to g7gpr/RMS
 cd ~/source/RMS
-mkdir -p /tmp/gmnbackup/$(whoami)
-cp .config /tmp/gmnbackup/$(whoami)/.
-cp platepar_cmn2010.cal /tmp/gmnbackup/$(whoami)/.
-cp mask.bmp /tmp/gmnbackup/$(whoami)/.
+mkdir -p ~/gmnbackup/$(whoami)
+cp .config ~/gmnbackup/$(whoami)/.
+cp platepar_cmn2010.cal ~/gmnbackup/$(whoami)/.
+cp mask.bmp  ~/gmnbackup/$(whoami)/.
 cd ~/source
 mv ~/source/RMS ~/source/RMS_backup
 git clone https://github.com/g7gpr/RMS
 cd ~/source/RMS
-cp /tmp/gmnbackup/$(whoami)/.config .
-cp /tmp/gmnbackup/$(whoami)/platepar_cmn2010.cal .
-cp /tmp/gmnbackup/$(whoami)/mask.bmp .
+cp ~/gmnbackup/$(whoami)/.config .
+cp ~/gmnbackup/$(whoami)/platepar_cmn2010.cal .
+cp ~/gmnbackup/$(whoami)/mask.bmp .
 Scripts/RMS_Update.sh
 git remote -v
 
