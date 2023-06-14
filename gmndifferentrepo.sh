27c27ad816b61430a44237109cbb02b8cce70a72
David Rollinson <g7gpr@outlook.com>
2023-06-14 21:18:05 +0000

    Set origin after changing repo

--- gmndifferentrepo.sh
+++ gmndifferentrepo.sh
@@ -7,9 +7,10 @@ cp platepar_cmn2010.cal ~/gmnbackup/$(whoami)/.
 cp mask.bmp  ~/gmnbackup/$(whoami)/.
 cd ~/source
 mv ~/source/RMS ~/source/RMS_backup
-git clone https://github.com/CroatianMeteorNetworkRMS
+git clone https://github.com/CroatianMeteorNetwork/RMS
 cd ~/source/RMS
 Scripts/RMS_Update.sh
+git remote set-url origin https://github.com/CroatianMeteorNetwork/RMS
 git remote -v
 rm ~/source/RMS/.config
 rm ~/source/RMS/mask.bmp
@@ -18,5 +19,6 @@ git checkout eventmonitor
 cp ~/gmnbackup/$(whoami)/.config .
 cp ~/gmnbackup/$(whoami)/mask.bmp . 
 cp ~/gmnbackup/$(whoami)/platepar_cmn2010.cal . 
+
 
 git branch
