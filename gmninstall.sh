410101c98f25ae58a492014676ef3d2d44fd9bc1
rollinson <g7gpr@outlook.com>
2024-05-28 21:15:52 +0800

    Modifications

--- gmninstall.sh
+++ gmninstall.sh
@@ -6,10 +6,11 @@
 cd ~
 
 
+
 #Clean up before we start
 rm install.sh
 rm opencv4_install.sh
-rm -rf source
+sudo rm -rf source
 
 #get the files
 #wget https://gist.githubusercontent.com/edharman/24f2d8a9a6c475057e68b2f490da9632/raw/3e7d2f18c45aba04ab015ba46d17f242ece2e4e8/install.sh 
@@ -17,12 +18,14 @@ rm -rf source
 #wget https://gist.githubusercontent.com/edharman/24f2d8a9a6c475057e68b2f490da9632/raw/eafd0120064641b67deeb9bde133baf0b2a12970/install.sh
 #wget https://gist.githubusercontent.com/edharman/96ebcd7809c3f4b85e72781145d03e0f/raw/2d936f2b52e1b7a3ec4f77d52d425c6e9c4050cf/opencv4_install.sh 
 
-https://gist.githubusercontent.com/edharman/24f2d8a9a6c475057e68b2f490da9632/raw/eafd0120064641b67deeb9bde133baf0b2a12970/install.sh
-https://gist.githubusercontent.com/edharman/96ebcd7809c3f4b85e72781145d03e0f/raw/2d936f2b52e1b7a3ec4f77d52d425c6e9c4050cf/opencv4_install.sh
+wget https://gist.githubusercontent.com/edharman/24f2d8a9a6c475057e68b2f490da9632/raw/eafd0120064641b67deeb9bde133baf0b2a12970/install.sh
+wget https://gist.githubusercontent.com/edharman/96ebcd7809c3f4b85e72781145d03e0f/raw/2d936f2b52e1b7a3ec4f77d52d425c6e9c4050cf/opencv4_install.sh
 
 #Make them executable
 chmod +x *.sh
 
+#install pip
+sudo apt-get install pip -y
 #install curl - this is needed by istrastream but does not always get installed
 sudo apt-get install curl -y
 
@@ -41,7 +44,7 @@ sudo apt-get install bc -y
 
 #Do the installation
 ./install.sh
-
+mkdir -p /home/gmn/cameras
 
 #Clean up
 #rm install.sh
