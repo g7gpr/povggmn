a6a1cf540a607113d61d129dea86046b7619d09d
g7gpr <g7gpr@outlook.com>
2024-07-29 11:22:13 +0800

    Start work on SkyMap

--- gmnFOVSkyMap.sh
+++ gmnFOVSkyMap.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-for camera in /home/gmn/cameras/
+for camera in /home/gmn/cameras/*
 do
-  echo $camera
-  command_string = " " + $camera + " "
+  echo $(basename $camera)
+  platepar_path="/home/$(basename $camera)/source/RMS/platepar_cmn2010.cal"
+  mkdir -p /home/gmn/platepars/$(basename $camera)/
+  cp $platepar_path /home/gmn/platepars/$(basename $camera)/
 done
 
 echo $command_string
