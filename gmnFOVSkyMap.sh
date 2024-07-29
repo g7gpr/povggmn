ed3f33dddc3102cb00ca438eb8f9cac338b6b572
g7gpr <g7gpr@outlook.com>
2024-07-29 12:11:09 +0800

    Save in RMS_data/$hostcd

--- gmnFOVSkyMap.sh
+++ gmnFOVSkyMap.sh
@@ -12,9 +12,9 @@ done
 
 cd ~/source/RMS/
 
-mkdir -p /home/$username/RMS_data/SkyMaps/$(hostname)
+mkdir -p /home/$username/RMS_data/SkyMaps/
 python -m Utils.FOVSkyMap -n /home/gmn/platepars/
-target_dir=/home/$username/RMS_data/SkyMaps/$(hostname)
+target_dir=/home/$username/RMS_data/SkyMaps/$(hostname).png
 mv /home/$username/platepars/fov_sky_map.png $target_dir
 echo Saved at
 echo $target_dir
