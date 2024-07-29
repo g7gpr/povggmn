c84bf311fd700de745cb231d455607214be60025
g7gpr <g7gpr@outlook.com>
2024-07-29 12:50:15 +0800

    Create directories

--- gmnFOVSkyMap.sh
+++ gmnFOVSkyMap.sh
@@ -8,10 +8,11 @@ target_dir=/home/$username/RMS_data/SkyMaps/$(hostname).png
 for camera in /home/gmn/cameras/*
 do
   platepar_path="/home/$(basename $camera)/source/RMS/platepar_cmn2010.cal"
-  mask_path= "/home/$(basename $camera)/source/RMS/mask.bmp"
+  mask_path="/home/$(basename $camera)/source/RMS/mask.bmp"
   mkdir -p /home/$username/platepars/$(basename $camera)/
   cp $platepar_path /home/$username/platepars/$(basename $camera)/
   cp $mask_path     /home/$username/platepars/$(basename $camera)/
+  mkdir -p $target_dir/$camera/
   cp $platepar_path $target_dir/$camera/
   cp $mask_path     $target_dir/$camera/
 done
