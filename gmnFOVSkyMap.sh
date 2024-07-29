1fee19314f765ff194a71eb546adfcf9916abab0
g7gpr <g7gpr@outlook.com>
2024-07-29 12:47:16 +0800

    Get masks as well

--- gmnFOVSkyMap.sh
+++ gmnFOVSkyMap.sh
@@ -2,25 +2,25 @@
 
 #get username for paths
 username=$(whoami)
-
+target_dir=/home/$username/RMS_data/SkyMaps/$(hostname).png
 #copy masks and platepar for all cameras
 
 for camera in /home/gmn/cameras/*
 do
-
   platepar_path="/home/$(basename $camera)/source/RMS/platepar_cmn2010.cal"
   mask_path= "/home/$(basename $camera)/source/RMS/mask.bmp"
   mkdir -p /home/$username/platepars/$(basename $camera)/
   cp $platepar_path /home/$username/platepars/$(basename $camera)/
   cp $mask_path     /home/$username/platepars/$(basename $camera)/
-
+  cp $platepar_path $target_dir/$camera/
+  cp $mask_path     $target_dir/$camera/
 done
 
 cd ~/source/RMS/
 
 mkdir -p /home/$username/RMS_data/SkyMaps/
 python -m Utils.FOVSkyMap -n /home/gmn/platepars/
-target_dir=/home/$username/RMS_data/SkyMaps/$(hostname).png
+
 mv /home/$username/platepars/fov_sky_map.png $target_dir
 echo Saved at
 echo $target_dir
