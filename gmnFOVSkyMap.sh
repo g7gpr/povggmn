9eb133bab836241a1cee221a27cbc5b4a80c77ad
g7gpr <g7gpr@outlook.com>
2024-07-29 11:46:18 +0800

    Reduce print calls

--- gmnFOVSkyMap.sh
+++ gmnFOVSkyMap.sh
@@ -2,17 +2,17 @@
 
 #get username for paths
 username=$(whoami)
-echo $username
 for camera in /home/gmn/cameras/*
 do
-  echo Copying $(basename $camera)
+
   platepar_path="/home/$(basename $camera)/source/RMS/platepar_cmn2010.cal"
   mkdir -p /home/$username/platepars/$(basename $camera)/
   cp $platepar_path /home/$username/platepars/$(basename $camera)/
 done
 
 cd source/RMS
-pwd
+
 mkdir -p /home/$username/RMS_data/SkyMap
 python -m Utils.FOVSkyMap -n /home/gmn/platepars/
 mv /home/$username/platepars/fov_sky_map.png /home/$username/RMS_data/SkyMap
+echo "File at /home/$username/RMS_data/SkyMap/"
