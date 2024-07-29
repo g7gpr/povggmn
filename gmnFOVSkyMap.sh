bbaf79e4f5fa68329ef87de2a7b855ad7307d059
g7gpr <g7gpr@outlook.com>
2024-07-29 11:44:30 +0800

    Fix path dependency

--- gmnFOVSkyMap.sh
+++ gmnFOVSkyMap.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
+#get username for paths
 username=$(whoami)
 echo $username
 for camera in /home/gmn/cameras/*
 do
   echo Copying $(basename $camera)
   platepar_path="/home/$(basename $camera)/source/RMS/platepar_cmn2010.cal"
-  mkdir -p /home/gmn/platepars/$(basename $camera)/
-  cp $platepar_path /home/gmn/platepars/$(basename $camera)/
+  mkdir -p /home/$username/platepars/$(basename $camera)/
+  cp $platepar_path /home/$username/platepars/$(basename $camera)/
 done
 
 cd source/RMS
 pwd
-mkdir -p /home/gmn/RMS_data/SkyMap
+mkdir -p /home/$username/RMS_data/SkyMap
 python -m Utils.FOVSkyMap -n /home/gmn/platepars/
-mv /home/gmn/platepars/fov_sky_map.png /home/gmnplatepars
+mv /home/$username/platepars/fov_sky_map.png /home/$username/RMS_data/SkyMap
