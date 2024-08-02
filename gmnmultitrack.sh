7dc07ceea0203f4b0c7747316f901fa8fb648322
David Rollinson <g7gpr@outlook.com>
2024-08-02 08:44:36 +0000

    send to web from own user area

--- gmnmultitrack.sh
+++ gmnmultitrack.sh
@@ -26,7 +26,9 @@ echo $captured
 #commandstring=$commandstring'   '$captured
 done
 cd ~/source/RMS
-commandstring='python Utils/TrackStack.py '$commandstring' --constellations  -c . -f6 -o /home/gmn/sendtoweb '$1
+commandstring='python Utils/TrackStack.py '$commandstring' --constellations  -c . -f6 -o ~/sendtoweb '$1
 echo $commandstring
+mkdir -p ~/sendtoweb
 $commandstring
-rsync /home/gmn/sendtoweb/*_track* gmndata@192.168.1.230:/home/gmndata/trackstacks/$(hostname)/
+
+rsync ~/sendtoweb/*_track* gmndata@192.168.1.230:/home/gmndata/trackstacks/$(hostname)/
