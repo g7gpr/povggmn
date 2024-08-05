3d0df2873df5411327318c867947330280a7723b
David Rollinson <g7gpr@outlook.com>
2024-08-05 06:20:58 +0000

    dirscan

--- dirscan.sh
+++ dirscan.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 for cam in /home/gmn/cameras/*
  do
- echo $cam    $(ls /home/$(basename $cam)/RMS_data/CapturedFiles/ | wc -l)
+ echo $(basename cam)    $(ls /home/$(basename $cam)/RMS_data/CapturedFiles/ | wc -l)
 done
 df -h | grep 'dev/'
