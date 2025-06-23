b98ba0497593594c67fecbcabd24dfe54c25ca8f
g7gpr <g7gpr@outlook.com>
2025-06-23 08:33:14 +0000

    Upload .tar files with no checks

--- gmntaruploader.sh
+++ gmntaruploader.sh
@@ -51,128 +51,13 @@ fi
 
 done
 
-if [ -z "$filepath" ];
-then
-echo "No files without confirmation"
-logger -s -t $(whoami) Nothing to upload to $remote
-rm ~/.taruploaderrunning
-exit
-else
-echo "File needs to be confirmed"
-fi
-
-#check to see if remote server incoming space is free
-
-filesinincoming=$(sftp $remote <<<"ls files/AU*.tar" | grep $cameraname | wc -l)
-
-echo Files in incoming : $filesinincoming
-
-if [ $filesinincoming -eq 0 ];
-then
-echo Incoming is empty
-else
-echo Incoming is not empty - which is strange, why is the file still there? Is it corrupted?
-
-sftp $remote <<< "ls -la files/AU*.tar" 1> fileinincoming
-
-echo File in incoming
-incomingfilesize=$(more fileinincoming |   awk '{print  $5}')
-
-incomingfilepath=$(more fileinincoming |   awk '{print  $9}')
-echo Filepath $incomingfilepath
-
-incomingfilename=${incomingfilepath##*/}
-echo Incoming filename is $incomingfilename
-
-localfilesize=$(wc -c $filepath | cut -d" " -f1)
-echo "Remote filesize :"$incomingfilesize
-echo "Local filesize  : "$localfilesize
-
-if [ $incomingfilesize -eq $localfilesize ] ;
-then
-echo Local and remote file sizes are the same. Nothing do do.
-touch $filepath.uploaded
-else
-echo Local and remote file sizes are different. Do not remove corrupt file from incoming as this causes problems.
-
 touch $filepath.uploading
 sftp gmn.uwo.ca <<END
 cd files
 put $filepath
 END
 rm $filepath.uploading
-touch $filepath.uploaded
-logger -s -t $(whoami) Uploaded $unconfirmedfilename because it was not found on $remote
-rm ~/.taruploaderrunning
-exit 2
-
-
-fi
-
-
-
-rm ~/.taruploaderrunning
-exit 1
-fi
-
-
-echo Filepath is $filepath
-unconfirmedfilename=${filepath##*/}
-
-localfilesize=$(wc -c $filepath | cut -d" " -f1)
-
-echo Remote file size is $incomingfilesize
-echo Local file size is  $localfilesize
-
-#Get the size of the file on the remote machine
-
-echo Look for $unconfirmedfilename on $remote
-remotefilesize=$(sftp $remote <<< "ls -la files/processed/*.tar" | grep $unconfirmedfilename |   awk '{print  $5}')
-echo $unconfirmedfilename remote filesize is $remotefilesize
-
-if [ -z "$remotefilesize" ];
-then
-echo "File probably does not exist, make an upload"
-#upload goes here
-touch $filepath.uploading
-sftp gmn.uwo.ca <<END
-cd files
-put $filepath
-END
-rm $filepath.uploading
-echo Create uploaded marker $filepath.uploaded
-touch $filepath.uploaded
-logger -s -t $(whoami) Uploaded $unconfirmedfilename because it was not found on $remote
-rm ~/.taruploaderrunning
-exit 2
-fi
-
-
-#Get the local size
-
-localfilesize=$(wc -c $filepath | cut -d" " -f1)
-
-echo $unconfirmedfilename " local filesize is"  $localfilesize
-
-if [ $localfilesize -eq $remotefilesize ];
-then
-echo "File was uploaded successfully"
 touch $filepath.confirmed
-rm $filepath.uploaded
-logger -s -t $unconfirmedfilename was found with the correct size at $remote
-else
-echo "Local file size is different to remote file size, make an upload"
-#upload goes here
-touch $filepath.uploading
-sftp gmn.uwo.ca <<END
-cd files
-put $filepath
-END
-rm $filepath.uploading
-logger -s -t $(whoami) Uploaded $unconfirmedfilename because filesize was different at $remote
-rm ~/.taruploaderrunning
-exit 3
-fi
-
 
 rm ~/.taruploaderrunning
+exit 2
