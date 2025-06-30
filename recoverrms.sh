04a550a9f06bfb43a6ed92c81ef0c59110c331ad
g7gpr <g7gpr@outlook.com>
2025-06-30 09:02:10 +0000

    Give info at end

--- recoverrms.sh
+++ recoverrms.sh
@@ -3,3 +3,5 @@ cd ~/source/RMS
 last_archive=$(stat ~/RMS_data/CapturedFiles/* | grep File: | tail -n1 | cut -d ':' -f2)
 echo Pulling files from $last_archive
 cp last_archive/mask.bmp last_archive/.config last_archive/platepar_cmn2010.cal .
+echo .config file has $(grep stationID .config)
+echo platepar file has $(grep station_code platepar_cmn2010.cal)
