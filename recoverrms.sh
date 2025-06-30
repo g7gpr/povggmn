69efc3820e3c0bd33feaf882a1940615d2495f7f
g7gpr <g7gpr@outlook.com>
2025-06-30 09:18:43 +0000

    Use archived files

--- recoverrms.sh
+++ recoverrms.sh
@@ -4,7 +4,7 @@ rm -rf RMS
 rsync -ah --include ".*" /home/gmn/source/RMS .
 git checkout prerelease
 cd ~/source/RMS
-last_archive=$(stat ~/RMS_dats/ArchivedFiles/* | grep File: | tail -n1 | cut -d ':' -f2)
+last_archive=$(stat ~/RMS_data/ArchivedFiles/* | grep File: | tail -n1 | cut -d ':' -f2)
 echo Pulling files from $last_archive
 cp ${last_archive}/mask.bmp ${last_archive}/.config ${last_archive}/platepar_cmn2010.cal .
 python -m Utils.MigrateConfig -u
