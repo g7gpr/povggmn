124cddcbf327911a752efe45e46eae6a73f1a070
g7gpr <g7gpr@outlook.com>
2025-06-30 09:42:00 +0000

    Check for not XX0001

--- recoverrms.sh
+++ recoverrms.sh
@@ -6,7 +6,13 @@ cd ~/source/RMS
 mv .config .configTemplate
 git reset --hard
 git checkout prerelease
-for dir in ~/RMS_data/ArchivedFiles/*/; do last_archive=$dir; done
+for dir in ~/RMS_data/ArchivedFiles/*/; do
+    last_archive=$dir;
+    stationID=$(grep stationID ${last_archive}.config | cut -d ":" -f2)
+    echo ${staionID}
+    if [[ stationID != "XX0001"]]; then
+      break
+    done
 echo Pulling files from ${last_archive}
 cp ${last_archive}mask.bmp ${last_archive}.config ${last_archive}platepar_cmn2010.cal .
 python -m Utils.MigrateConfig -u
