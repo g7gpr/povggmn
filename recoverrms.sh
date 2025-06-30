5f2b25a08ac33e1fca6521da0eeeb056c4d626da
g7gpr <g7gpr@outlook.com>
2025-06-30 09:34:08 +0000

    Run RMS Update

--- recoverrms.sh
+++ recoverrms.sh
@@ -2,14 +2,14 @@
 cd ~/source
 rm -rf RMS
 rsync -ah --include ".*" /home/gmn/source/RMS .
+cd ~/source/RMS
+Scripts/RMS_Update.sh
 git reset --hard
 git checkout prerelease
-cd ~/source/RMS
 for dir in ~/RMS_data/ArchivedFiles/*/; do last_archive=$dir; done
 echo Pulling files from ${last_archive}
 cp ${last_archive}mask.bmp ${last_archive}.config ${last_archive}platepar_cmn2010.cal .
 python -m Utils.MigrateConfig -u
 echo .config file has $(grep stationID .config | cut -d ":" -f2 )
 echo platepar file has $(grep station_code platepar_cmn2010.cal | cut -d ":" -f2)
-cd ~/source
-cd RMS
+
