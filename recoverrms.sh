a042c522f5c4485c3aa62d5a636e0d475c326401
g7gpr <g7gpr@outlook.com>
2025-06-30 09:00:02 +0000

    add recoverrms

---
+++ recoverrms.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+last_archive=$(stat ~/RMS_data/CapturedFiles/* | grep File: | tail -n1 | cut -d ':' -f2)
+echo Pulling files from $last_archive
+cp last_archive/mask.bmp last_archive/.config last_archive/platepar_cmn2010.cal .
