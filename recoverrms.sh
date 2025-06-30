6fe33de7df9ffc1db16aa0ca7e7a754757a77a83
g7gpr <g7gpr@outlook.com>
2025-06-30 09:25:29 +0000

    Use archived files

--- recoverrms.sh
+++ recoverrms.sh
@@ -8,5 +8,5 @@ for dir in ~/RMS_data/ArchivedFiles/*/; do last_archive=$dir; done
 echo Pulling files from ${last_archive}
 cp ${last_archive}mask.bmp ${last_archive}.config ${last_archive}platepar_cmn2010.cal .
 python -m Utils.MigrateConfig -u
-echo .config file has $(grep stationID .config)
-echo platepar file has $(grep station_code platepar_cmn2010.cal)
+echo .config file has $(grep stationID .config | cut ":" -f2 )
+echo platepar file has $(grep station_code platepar_cmn2010.cal | cut ":" -f2)
