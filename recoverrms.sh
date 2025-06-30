bf283750b57371a89bfaa60306350a6ce2b52502
g7gpr <g7gpr@outlook.com>
2025-06-30 09:14:03 +0000

    Delete and recreate

--- recoverrms.sh
+++ recoverrms.sh
@@ -6,7 +6,7 @@ git checkout prerelease
 cd ~/source/RMS
 last_archive=$(stat ~/RMS_data/CapturedFiles/* | grep File: | tail -n1 | cut -d ':' -f2)
 echo Pulling files from $last_archive
-cp $(last_archive)/mask.bmp $(last_archive)/.config $(last_archive)/platepar_cmn2010.cal .
+cp ${last_archive}/mask.bmp ${last_archive}/.config ${last_archive}/platepar_cmn2010.cal .
 python -m Utils.MigrateConfig -u
 echo .config file has $(grep stationID .config)
 echo platepar file has $(grep station_code platepar_cmn2010.cal)
