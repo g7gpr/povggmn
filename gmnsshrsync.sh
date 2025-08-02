836758e482741cb45bd98f6ca6205e7b35743e62
g7gpr <g7gpr@outlook.com>
2025-08-02 11:47:44 +0000

    Delete files on remote

--- gmnsshrsync.sh
+++ gmnsshrsync.sh
@@ -9,8 +9,8 @@ echo Number of rsync running lines $rsyncrunning
 if [ $rsyncrunning -eq "0" ];
 then
 echo rsync is not running - start it
-rsync --progress -av -e "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" --bwlimit=1024  --partial-dir=partial/ ~/RMS_data/FramesFiles/*.tar $(whoami)@192.168.1.241:files/incoming
-rsync --progress -av -e "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" --bwlimit=1024  --partial-dir=partial/ ~/RMS_data/ArchivedFiles/*.bz2 $(whoami)@192.168.1.241:files/incoming
+rsync --progress -av -e --delete "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" --bwlimit=1024  --partial-dir=partial/ ~/RMS_data/FramesFiles/*.tar $(whoami)@192.168.1.241:files/incoming
+rsync --progress -av -e --delete "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" --bwlimit=1024  --partial-dir=partial/ ~/RMS_data/ArchivedFiles/*.bz2 $(whoami)@192.168.1.241:files/incoming
 logger -s -t rsync started
 else
 echo rsync is running - do nothing
