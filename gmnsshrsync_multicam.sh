4edade03a8d37c302af0e51e759ccbb2cf47dee7
g7gpr <g7gpr@outlook.com>
2026-02-04 12:19:53 +0000

    Put gmnsshrysnc back in

---
+++ gmnsshrsync_multicam.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#Script to start rsync if it is not already running
+
+
+#use ps to discover if rsync is running in this user account
+rsyncrunning=$(ps -x | grep rsync | grep $(whoami) | wc -l)
+whoami_upper="${USER^^}"
+
+echo Number of rsync running lines $rsyncrunning
+if [ $rsyncrunning -eq "0" ];
+then
+echo rsync is not running - start it
+rsync --progress -av -e "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" --bwlimit=1024  --partial-dir=partial/ /home/rms/RMS_data/ArchivedFiles/*.bz2 192.168.1.241:files/incoming
+rsync --progress -av -e "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" --bwlimit=1024  --partial-dir=partial/ ~/RMS_data/FramesFiles/*.tar 192.168.1.241:files/incoming
+logger -s -t rsync started
+else
+echo rsync is running - do nothing
+fi
+
