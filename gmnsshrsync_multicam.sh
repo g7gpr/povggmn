68fac46ce3f79bc5124266f22c5097c5fb78e9f9
g7gpr <g7gpr@outlook.com>
2026-02-04 12:42:07 +0000

    send metadata first

--- gmnsshrsync_multicam.sh
+++ gmnsshrsync_multicam.sh
@@ -11,7 +11,8 @@ echo Number of rsync running lines $rsyncrunning
 if [ $rsyncrunning -eq "0" ];
 then
 echo rsync is not running - start it
-rsync --progress -av -e "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" --bwlimit=1024  --partial-dir=partial/ /home/rms/RMS_data/${whoami_upper}/ArchivedFiles/*.bz2 192.168.1.241:files/incoming
+rsync --progress -av -e "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" --bwlimit=1024  --partial-dir=partial/ /home/rms/RMS_data/${whoami_upper}/ArchivedFiles/*_metadata.tar.bz2 192.168.1.241:files/incoming
+rsync --progress -av -e "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" --bwlimit=1024  --partial-dir=partial/ /home/rms/RMS_data/${whoami_upper}/ArchivedFiles/*_imgdata.tar.bz2.bz2 192.168.1.241:files/incoming
 rsync --progress -av -e "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" --bwlimit=1024  --partial-dir=partial/ /home/rms/RMS_data/${whoami_upper}/FramesFiles/*.tar 192.168.1.241:files/incoming
 logger -s -t rsync started
 else
